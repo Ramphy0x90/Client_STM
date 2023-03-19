13a8dcc1aa64f22893bb6ca6478803530603bbfa
ramphy0x90 <ramphy11@hotmail.com>
2023-03-19 21:29:05 +0100

    Update main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ItemCardComponent } from './components/item-card/item-card.component';
+import { PriceCardComponent } from './components/price-card/price-card.component';
+import { ServicesComponent } from './containers/services/services.component';
+import { PricesComponent } from './containers/prices/prices.component';
+import { ContactComponent } from './containers/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ItemCardComponent,
+    PriceCardComponent,
+    ServicesComponent,
+    PricesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
