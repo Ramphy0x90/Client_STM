ad160070add642852966223094b6843e27f3cc98
ramphy0x90 <ramphy11@hotmail.com>
2023-03-26 14:29:33 +0200

    Update header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { NavOption } from 'src/app/models/navOption';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  navOptions: NavOption[] = [
+    { id: 'nav__opt_home', name: 'Home', route: 'home' },
+    { id: 'nav_opt_services', name: 'Servizi', route: 'servizi' },
+    { id: 'nav_opt_prices', name: 'Prezzi', route: 'prezzi' },
+    { id: 'nav_opt_contact', name: 'Contattaci', route: 'contattaci' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
