415da7e94ba4c9cb5f0cd9a1677693197e44ca17
ramphy0x90 <ramphy11@hotmail.com>
2023-03-26 14:23:55 +0200

    Update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './containers/contact/contact.component';
+import { HomeComponent } from './containers/home/home.component';
+import { PricesComponent } from './containers/prices/prices.component';
+import { ServicesComponent } from './containers/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'servizi', component: ServicesComponent },
+  { path: 'prezzi', component: PricesComponent },
+  { path: 'contattaci', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
